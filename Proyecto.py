4ced770e3f54a129379e8c6f9593b0af0f11951d
nelsonbolanoscr <42462224+nelsonbolanoscr@users.noreply.github.com>
2020-02-18 20:26:58 -0600

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -7,17 +7,12 @@ import csv
 
 files = listdir("C:/Users/NelsonEnriqueBolanos/Desktop/Python/Proyecto")
 
-with open('C:/Users/NelsonEnriqueBolanos/Desktop/Python/Move/testfinal.csv', 'w') as x:
-    write = csv.writer(x)
-
-    for i in files:
-        print(i)
-        with open('C:/Users/NelsonEnriqueBolanos/Desktop/Python/Proyecto/' + str(i)) as f:
-            reader = csv.reader(f)
-            test = list(reader)
-            print(test)
-        with open('C:/Users/NelsonEnriqueBolanos/Desktop/Python/Proyecto/Move/testfinal.csv', 'a', newline='') as p:
-            writer = csv.writer(p)
-            test2 = [
-                [test]
-            ]
+for i in files:
+    print(i)
+    with open('C:/Users/NelsonEnriqueBolanos/Desktop/Python/Proyecto/' + str(i)) as f:
+        reader = csv.reader(f)
+        test = list(reader)
+        print(test)
+    with open('C:/Users/NelsonEnriqueBolanos/Desktop/Python/Proyecto/Move/testfinal.csv', 'a', newline='') as p:
+        writer = csv.writer(p)
+        test2 = list(test)
